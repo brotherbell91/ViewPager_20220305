afefc56d69994437c996a50ead2b7c00914dbf1c
이형종 <hj36879@naver.com>
2022-03-05 14:45:53 +0900

    뷰페이져 어댑터의 실제 기능 구현

--- app/src/main/java/com/hyeongjong/viewpager_20220305/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/hyeongjong/viewpager_20220305/adapters/MainViewPagerAdapter.kt
@@ -3,13 +3,27 @@ package com.hyeongjong.viewpager_20220305.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.hyeongjong.viewpager_20220305.fragments.HelloFragment
+import com.hyeongjong.viewpager_20220305.fragments.MyInfoFragment
+import com.hyeongjong.viewpager_20220305.fragments.MyNameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+//      몇 페이지의 프래그먼트인지? return으로 표현
+        return 3
     }
 
     override fun getItem(position: Int): Fragment {
-        TODO("Not yet implemented")
+
+        if (position == 0) {
+            return MyNameFragment()
+        }
+        else if (position == 1){
+            return MyInfoFragment()
+        }
+        else {
+            return HelloFragment()
+        }
+
     }
 }
