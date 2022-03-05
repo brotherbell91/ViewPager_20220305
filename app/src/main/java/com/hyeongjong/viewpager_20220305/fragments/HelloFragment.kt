28307870352c56672de86bb699eff899bf046a17
이형종 <hj36879@naver.com>
2022-03-05 14:33:55 +0900

    2개의 프래그먼트 추가

---
+++ app/src/main/java/com/hyeongjong/viewpager_20220305/fragments/HelloFragment.kt
@@ -0,0 +1,26 @@
+package com.hyeongjong.viewpager_20220305.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hyeongjong.viewpager_20220305.R
+
+class HelloFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hello, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+
+    }
+}
