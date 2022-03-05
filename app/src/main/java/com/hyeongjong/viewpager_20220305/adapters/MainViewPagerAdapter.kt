18a7b148b5eaa1e5b94ed9833d5b421ed5b184b4
이형종 <hj36879@naver.com>
2022-03-05 15:33:55 +0900

    return when 문법 적용 예시

--- app/src/main/java/com/hyeongjong/viewpager_20220305/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/hyeongjong/viewpager_20220305/adapters/MainViewPagerAdapter.kt
@@ -11,6 +11,7 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
 
+
         if (position == 0) {
             return "이름"
         }
@@ -20,6 +21,7 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
         else {
             return "인사말"
         }
+
     }
 
     override fun getCount(): Int {
@@ -29,14 +31,10 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
 
-        if (position == 0) {
-            return MyNameFragment()
-        }
-        else if (position == 1){
-            return MyInfoFragment()
-        }
-        else {
-            return HelloFragment()
+        return when(position) {
+            0 -> MyNameFragment()
+            1 -> MyInfoFragment()
+            else -> HelloFragment()
         }
 
     }
