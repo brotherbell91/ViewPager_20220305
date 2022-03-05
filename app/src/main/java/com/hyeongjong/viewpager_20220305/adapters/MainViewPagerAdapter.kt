e03c2a9b85d31b285464e146ad996d1a3d21466c
이형종 <hj36879@naver.com>
2022-03-05 15:37:35 +0900

    페이지 제목도 return when 처리

--- app/src/main/java/com/hyeongjong/viewpager_20220305/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/hyeongjong/viewpager_20220305/adapters/MainViewPagerAdapter.kt
@@ -11,15 +11,10 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
 
-
-        if (position == 0) {
-            return "이름"
-        }
-        else if (position == 1) {
-            return "내 정보"
-        }
-        else {
-            return "인사말"
+        return when (position) {
+            0 -> "이름"
+            1 -> "내 정보"
+            else -> "인사말"
         }
 
     }
@@ -30,7 +25,18 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
     }
 
     override fun getItem(position: Int): Fragment {
-
+        
+//        if 문법
+//        if ( position == 0){
+//            return MyNameFragment()
+//        }
+//        else if (position == 1) {
+//            return MyInfoFragment()
+//        }
+//        else {
+//            return HelloFragment()
+//        }
+//        return when 문법(return 만 빼면 when 문법)
         return when(position) {
             0 -> MyNameFragment()
             1 -> MyInfoFragment()
